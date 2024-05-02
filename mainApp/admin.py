0ac9229c8007d85998c9aee950596ca3dfcfb479
= <=>
2024-05-02 22:27:56 +0500

    some changes

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Taom)
-admin.site.register(Ichimlik)
+
+class TaomAdmin(admin.ModelAdmin):
+    model = Taom
+    list_display = ('nom', 'narx')
+
+
+class IchimlikAdmin(admin.ModelAdmin):
+    model = Taom
+    list_display = ('nom', 'narx')
+
+
+admin.site.register(Taom, TaomAdmin)
+admin.site.register(Ichimlik, IchimlikAdmin)
